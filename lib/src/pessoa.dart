633a551115fa7e639832abc9f21d01434a6547de
Natanael Vieira <natanaelsales@live.com>
2022-08-12 19:02:26 -0300

    Update pessoa.dart
    Protegente variáveis usando '_' no início da variável e usando 'final' antes de declarar a variável.

--- lib/src/pessoa.dart
+++ lib/src/pessoa.dart
@@ -3,5 +3,9 @@ class Pessoa {
   var idade;
   var sexo;
 
+  //String _nomeLocal = "Nome qualquer"; // Quando uso uma variável começando com '_' eu digo para o programa que essa variável não é mais global, só consigo acessar ela aqui dentro desse escopo, não podendo ser acessado por outro local.
+
+  // final String otherName = "Nome qualquer"; // Já quando utilizo o final antes da variável eu estou dizendo que ela só pode ser adicionada apenas uma vez, não podendo ser adicionado um novo valor a ele novamente de outro local uma segunda ou terceira vezes, somente na primeira, depois disso ela fica inutilizada impedindo que seja adicionada indevidamente.
+
   Pessoa({this.nome, this.idade, this.sexo});
 }
